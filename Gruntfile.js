021fd494ce2d212bca9ffde20704316541d275f8
Francisco Fernandez Novella <NeuroMac@Alejandras-MacBook-Pro.local>
2015-02-22 15:22:24 -0300

    fix for headless test runing

--- Gruntfile.js
+++ Gruntfile.js
@@ -127,7 +127,10 @@ module.exports = function (grunt) {
         mocha: {
             all: {
                 options: {
-                    run: true,
+                    log: true,
+                    reporter: 'Spec',
+                    run: false,
+                    timeout: 10000,
                     urls: ['http://localhost:<%= connect.test.options.port %>/index.html']
                 }
             }
